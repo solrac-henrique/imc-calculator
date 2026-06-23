f458711b62aef9bd7b3d8bc4aa508c3ba86e12a2
Carlos Henrique <ch08082003@gmail.com>
2026-06-23 13:18:16 -0300

    Create initAccordion.js

---
+++ js/modules/initAccordion.js
@@ -0,0 +1,17 @@
+export default function Iniaccordion(){
+  const dt = document.querySelectorAll('.js-accordion dt')
+  const ativo = 'ativo' 
+  dt[0].nextElementSibling.classList.add(ativo)
+  dt[0].classList.add(ativo)
+
+
+  function addAtivo(e){
+  const dt = e.target
+  const nextdd = e.target.nextElementSibling
+  nextdd.classList.toggle(ativo)
+  dt.classList.toggle(ativo)
+
+  }
+
+  dt.forEach((i)=> i.addEventListener('click', addAtivo))
+  }
